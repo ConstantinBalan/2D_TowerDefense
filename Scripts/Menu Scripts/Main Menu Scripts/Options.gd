98525265d14b90ddc849582aac6d05f4eadb7ba3
Constantin Balan <35816870+ConstantinBalan@users.noreply.github.com>
2024-06-24 21:54:05 -0400

    Reorganized scene locations

--- Scripts/Menu Scripts/Main Menu Scripts/Options.gd
+++ Scripts/Menu Scripts/Main Menu Scripts/Options.gd
@@ -12,4 +12,4 @@ func _process(delta):
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/options_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/Menus/options_menu.tscn")
--- Scripts/Menu Scripts/Main Menu Scripts/StartGame.gd
+++ Scripts/Menu Scripts/Main Menu Scripts/StartGame.gd
@@ -12,4 +12,4 @@ func _process(delta):
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/world_select.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/Menus/world_select.tscn")
